7d39d348b0c962f9d3a2d344cc2719401bf16c0d
gumskul1 <gumskul59@gmail.com>
2020-01-28 11:33:18 +0000

    params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/home')
 def home():
-  return render_template("home.html")
+  return render_template("home.html", aktiva_lapa = 'home')
 
 @app.route('/about')
 def about():
@@ -17,6 +17,18 @@ def about():
 @app.route('/contact')
 def contact():
   return render_template('contact.html',phone=12345678)
+
+@app.route('/params')
+def params ():
+  args = request.args
+  for key, value in args.items():
+    print(f"{key}.{value}")
+  return args
+
+@app.route('/params_table')
+def params_table ():
+  args = request.args
+  return render_template('params_table.html', args = args)
 
 if __name__ =='__main__':
   app.run(host = '0.0.0.0', port = 5125, threaded = True, debug = True)
